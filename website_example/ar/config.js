c04739d6fceefb46af377166074c65b1b7e162b5
mohammed <m.schamboul@yahoo.com>
2017-11-10 11:01:05 +0300

    u

---
+++ website_example/ar/config.js
@@ -0,0 +1,16 @@
+var api = "https://electroneum-pool.me/api";
+
+var coinUnits = 100;
+var coinDecimalPlaces = 2;
+
+var poolHost = "https://electroneum-pool.me/ar";
+
+var irc = "";
+
+var email = "";
+
+var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+var blockchainExplorer = "https://blockexplorer.electroneum.com/block/";
+
+var transactionExplorer = "https://blockexplorer.electroneum.com/tx/";
